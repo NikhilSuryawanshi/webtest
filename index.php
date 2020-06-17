69bde9ebb31d29ae79333fab52f2f1f679840b6b
Nikhil Suryawanshi <nikhilsurya29@gmail.com>
2020-06-18 01:28:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html>
-<body bgcolor="cyan">
+  <head>
+    <title>Welcome to the Cloud </title>
+  </head>
+<body bgcolor="gray">
 
-<h1>Welcome to nik clouds</h1>
+<h1>"Things do not happen. Things are made to happen !" </h1>
 
 <pre>
 <?php
 
-print `ifconfig`;
+print `ifconfig enp0s3`;
 $file = file_get_contents('url.txt');
 echo $file;
 echo '<img src="'.$file.'"  width="500" height="600">';
